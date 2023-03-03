3cb4cb9804ec25189b826d05946c422ed6bbed26
Rainiel Ang <rainielang@yrwwt4gh6p.home>
2023-03-03 11:31:30 +0000

    Initial upload from the zip file

---
+++ azureproject/production.py
@@ -0,0 +1,41 @@
+from .settings import *
+import os
+import ast
+
+# Configure the domain name using the environment variable
+# that Azure automatically creates for us.
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+DEBUG = ast.literal_eval(os.getenv("DEBUG", False))
+
+# WhiteNoise configuration
+MIDDLEWARE = [                                                                   
+    'django.middleware.security.SecurityMiddleware',
+# Add whitenoise middleware after the security middleware                             
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',                      
+    'django.middleware.common.CommonMiddleware',                                 
+    'django.middleware.csrf.CsrfViewMiddleware',                                 
+    'django.contrib.auth.middleware.AuthenticationMiddleware',                   
+    'django.contrib.messages.middleware.MessageMiddleware',                      
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',                    
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# DBHOST is only the server name, not the full URL
+hostname = os.environ.get('DBHOST')
+print(os.environ.keys())
+# Configure Postgres database; the full username for PostgreSQL flexible server is
+# username (not @sever-name).
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DBNAME','django'),
+        'HOST': hostname + ".postgres.database.azure.com",
+        'USER': os.environ.get('DBUSER'),
+        'OPTIONS': {'sslmode':'require'},
+        'PASSWORD': os.environ.get('DBPASS')
+    }
+}
